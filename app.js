17cab4d1f602c092cafe08a1aa035fdf51e86a19
shichia22 <shichia0207@gmail.com>
2021-07-21 19:56:32 +0800

    feat:add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose') 
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/money', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}) 
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // index
 app.get('/', (req, res) => {
