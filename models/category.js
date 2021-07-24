2889796b569d265faf0d9c2208ccaa9f90cc1a45
shichia22 <shichia0207@gmail.com>
2021-07-24 22:13:09 +0800

    fix: category about Whaleep

--- models/category.js
+++ models/category.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const categorySchema = new Schema({
-  categoryName: {
+  title: {
     type: String,
-    required: true,
+    require: true,
   },
-  categoryIcon: {
+  icon: {
     type: String,
-    required: true,
   },
+  records: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Record',
+    },
+  ],
 })
-
 module.exports = mongoose.model('Category', categorySchema)
--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -2,27 +2,15 @@ const mongoose = require('mongoose')
 const Category = require('../category')
 
 let categoryData = [
-  {
-    categoryName: '家居物業',
-    categoryIcon: 'fas fa-home',
-  },
-  {
-    categoryName: '交通出行',
-    categoryIcon: 'fas fa-shuttle-van',
-  },
-  {
-    categoryName: '休閒娛樂',
-    categoryIcon: 'fas fa-grin-beam',
-  },
-  {
-    categoryName: '餐飲食品',
-    categoryIcon: 'fas fa-utensils',
-  },
-  {
-    categoryName: '其他',
-    categoryIcon: 'fas fa-pen',
-  },
-]
+  ['家居物業', 'fa-home'],
+  ['交通出行', 'fa-shuttle-van'],
+  ['休閒娛樂', 'fa-grin-beam'],
+  ['餐飲食品', 'fa-utensils'],
+  ['其他', 'fa-pen'],
+].map((category) => ({
+  title: category[0],
+  icon: category[1],
+}))
 
 mongoose.connect('mongodb://localhost/money', {
   useNewUrlParser: true,
@@ -31,15 +19,9 @@ mongoose.connect('mongodb://localhost/money', {
 
 const db = mongoose.connection
 
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
 db.once('open', () => {
-  Category.create(categoryData)
-    .then(() => {
-      console.log('Add category seeder done!')
-      return db.close()
-    })
-    .catch((err) => console.error(err))
+  Category.create(categorySeedData).then(() => {
+    console.log('Category seeder done.')
+    return db.close()
+  })
 })
