5ee69d5ebed5c338d72a8681d57a72a2b812b8ef
shichia22 <shichia0207@gmail.com>
2021-07-25 17:12:22 +0800

    feat: add delete record function

--- app.js
+++ app.js
@@ -56,11 +56,20 @@ app.post('/', (req, res) => {
 })
 
 // edit
-app.get('/todos/:id/edit', (req, res) => {
+app.get('/records/:id/edit', (req, res) => {
   const id = req.params.id
   return Record.findById(id)
     .lean()
     .then((record) => res.render('edit', { record }))
+    .catch((error) => console.log(error))
+})
+
+// delete
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then((record) => record.remove())
+    .then(() => res.redirect('/'))
     .catch((error) => console.log(error))
 })
 
