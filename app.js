69970c13225beaf3727c364f0650f13609a8051f
shichia22 <shichia0207@gmail.com>
2021-07-25 16:01:01 +0800

    feat:add creat route,but error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 const Record = require('./models/record')
@@ -24,6 +25,7 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // index
 app.get('/', (req, res) => {
@@ -38,6 +40,17 @@ app.get('/', (req, res) => {
     .lean()
     .then((categorys) => res.render('index', { categorys })) // 將資料傳給 index 樣板
     .catch((error) => console.error(error)) // 錯誤處理
+})
+
+// create
+app.get('/records/new', (req, res) => {
+  return res.render('new', { categorys })
+})
+
+app.post('/', (req, res) => {
+  Record.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
 })
 
 // port
